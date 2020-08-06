cc3e5bc445d5bbd5eed456c5a93f96553038f8ba
Denis <powerslime@ya.ru>
2020-08-07 00:46:02 +0300

    Add persistent state

--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,9 @@
-import { createStore } from "redux";
+import { compose, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import persistState from "redux-localstorage";
 
 import rootReducer from "./reducers";
+const enhancer = compose(persistState());
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools(enhancer));
 export default store;
